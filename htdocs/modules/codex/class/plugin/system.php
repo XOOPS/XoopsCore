23e0cd3d7f65c95f81dad0e8bbc5bd00ce387c0e
geekwright <richard@geekwright.com>
2013-09-30 21:19:47 -0500

    Stop reporting each codex file as a user post
    Sychronizing users was adding 14 posts to each user.

--- htdocs/modules/codex/class/plugin/system.php
+++ htdocs/modules/codex/class/plugin/system.php
@@ -1,13 +1,16 @@
 <?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 /**
  * Codex module
- *
- * You may not change or alter any portion of this comment or credits
- * of supporting developers from this source code or any supporting source code
- * which is considered copyrighted (c) material of the original comment or credit authors.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
  * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
@@ -16,7 +19,6 @@
  * @author          Laurent JEN (Aka DuGris)
  * @version         $Id$
  */
-
 class CodexSystemPlugin extends Xoops_Module_Plugin_Abstract implements SystemPluginInterface
 {
     /**
@@ -29,9 +31,9 @@ class CodexSystemPlugin extends Xoops_Module_Plugin_Abstract implements SystemPl
      */
     public function userPosts($uid)
     {
-        $xoops = Xoops::getInstance();
-        $count = count(XoopsLists::getFileListAsArray($xoops->path('modules/codex/')))-2;
-        return $count;
+        //$xoops = Xoops::getInstance();
+        //$count = count(XoopsLists::getFileListAsArray($xoops->path('modules/codex/')))-2;
+        return 0;
     }
 
     /**
