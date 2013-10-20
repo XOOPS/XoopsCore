15701aaa3c452b5862cee15998c1ba2a883fca27
geekwright <richard@geekwright.com>
2013-10-20 09:49:46 -0500

    Revert "patches to corrects bugs"
    This reverts commit d525d62daa7080d837dee2c580b03aeec7eb2e0e.
    
    This must go through a pull request for review.

--- htdocs/modules/smilies/preloads/core.php
+++ htdocs/modules/smilies/preloads/core.php
@@ -29,15 +29,6 @@ defined('XOOPS_ROOT_PATH') or die('Restricted access');
  */
 class SmiliesCorePreload extends XoopsPreloadItem
 {
-    public static function initialize()
-    {
-        $path = dirname(dirname(__FILE__));
-        XoopsLoad::addMap(array(
-			'smilies' => $path . '/class/helper.php',
-			'smiliessmiley' => $path . '/class/smiley.php',
-			));
-    }
-	
     static function eventCoreIncludeCommonEnd($args)
     {
         $path = dirname(dirname(__FILE__));
@@ -85,4 +76,3 @@ class SmiliesCorePreload extends XoopsPreloadItem
         return true;
     }
 }
-SmiliesCorePreload::initialize();
