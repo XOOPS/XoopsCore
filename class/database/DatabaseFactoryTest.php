1131a997121e7e5c7da91107172b5ba46c4fcd8e
alain91 <alain091@gmail.com>
2013-10-22 11:49:08 +0200

    delete simpletest files and modify databasefactory to Doctrine

--- class/database/DatabaseFactoryTest.php
+++ class/database/DatabaseFactoryTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
 require_once(dirname(__FILE__).'/../../init_mini.php');
 
 require_once(XOOPS_ROOT_PATH.'/class/xoopsload.php');
@@ -11,21 +15,27 @@ class DatabaseFactoryTest extends MY_UnitTestCase
 {
     protected $myclass = 'XoopsDatabaseFactory';
     
-    public function SetUp() {
+    public function SetUp()
+	{
     }
 	
-    public function test_100() {
+    public function test_100()
+	{
 		$instance = XoopsDatabaseFactory::getDatabaseConnection();
 		$this->assertInstanceOf('XoopsMySQLDatabaseProxy', $instance);
 		$instance2 = XoopsDatabaseFactory::getDatabaseConnection();
 		$this->assertSame($instance, $instance2);
 		$this->assertInstanceOf('XoopsConnection', $instance->conn);
+		$driver = $instance->conn->getDriver();
+		$driver_conn = $driver->connect();
+		$this->assertInstanceOf('\Doctrine\DBAL\Driver\PDOConnection', $driver_conn);
         $this->assertSame(XOOPS_DB_PREFIX.'_test', $instance->prefix('test'));
         $this->assertSame(XOOPS_DB_PREFIX, $instance->prefix());
-
     }
 
-   /* public function test_200() {
+	/*
+	public function test_200()
+	{
         // removed because this function will be removed all together.
 		$instance = XoopsDatabaseFactory::getDatabase();
 		if (!defined('XOOPS_DB_PROXY'))
@@ -35,6 +45,7 @@ class DatabaseFactoryTest extends MY_UnitTestCase
 			
 		$instance2 = XoopsDatabaseFactory::getDatabase();
 		$this->assertSame($instance, $instance2);
-    } */
+    }
+	*/
 	
 }
