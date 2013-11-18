bfb198275d768e32d86a742489054a390130c062
geekwright <richard@geekwright.com>
2013-11-18 14:41:58 -0600

    Convert userconfigs to module from extension
    Module was not accessible by non-admin users, and there is currently no way to assign the needed access rights for extensions.

---
+++ htdocs/modules/userconfigs/admin/about.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @author          Richard Griffith <richard@geekwright.com>
+ * @version         $Id: about.php 8065 2011-11-06 02:02:32Z beckmi $
+ */
+
+require dirname(__FILE__) . '/admin_header.php';
+
+$aboutAdmin = new XoopsModuleAdmin();
+$aboutAdmin->displayNavigation('about.php');
+$aboutAdmin->displayAbout(true);
+
+require dirname(__FILE__) . '/admin_footer.php';
