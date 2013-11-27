ce09d4c39e3b8551787bef1f66f0244cfd2caff2
alain91 <alain091@gmail.com>
2013-11-27 21:55:17 +0100

    nouveau test

--- xoopsLib/Xoops/RegistryTest.php
+++ xoopsLib/Xoops/RegistryTest.php
@@ -16,45 +16,69 @@ class Xoops_RegistryTest extends MY_UnitTestCase
 	
     public function test_100()
 	{
-		// getInstance
+		$instance = Xoops_Registry::getInstance();
+		$this->assertInstanceOf($this->myclass, $instance);
+		
+		$instance1 = Xoops_Registry::getInstance();
+		$this->assertSame($instance1, $instance);
     }
 	
 	public function test_200()
 	{
-		// setInstance
+		$registry = new Xoops_Registry();
+		Xoops_Registry::_unsetInstance();
+		Xoops_Registry::setInstance($registry);
+		$instance = Xoops_Registry::getInstance();
+		$this->assertInstanceOf($this->myclass, $instance);
 	}
-	
-	public function test_300()
-	{
-		// init
-	}
-	
+
+	/**
+	* @expectedException PHPUnit_Framework_Error
+	*/	
 	public function test_400()
 	{
-		// setClassName
+		$x = Xoops_Registry::setClassName();
+		$this->assertFalse($x);
 	}
 	
-	public function test_500()
+	/**
+	* @expectedException PHPUnit_Framework_Error
+	*/	
+	public function test_420()
 	{
-		// _unsetInstance
+		Xoops_Registry::_unsetInstance();
+		$x = Xoops_Registry::setClassName(1);
+		$this->assertFalse($x);
+	}
+	
+	public function test_440()
+	{
+		Xoops_Registry::_unsetInstance();		
+		$x = Xoops_Registry::setClassName();
+		$this->assertTrue($x);
 	}
 	
 	public function test_600()
 	{
-		// get
+		// _unsetInstance
 	}
 	
 	public function test_700()
 	{
+		// get
+	}
+	
+	public function test_800()
+	{
 		// set
 	}
 
-	public function test_800()
+	public function test_900()
 	{
 		// isRegistered
 	}
 	
-	public function test_900()
+	public function test_1000()
 	{
 		// offsetExists
 	}
