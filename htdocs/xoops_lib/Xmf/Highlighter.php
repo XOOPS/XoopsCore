ed8512b2ef2ae7c4a3f33d6b7978b38bb6a67bea
geekwright <richard@geekwright.com>
2013-12-10 22:04:05 -0600

    Sync with 2.5 module version - update license links

--- htdocs/xoops_lib/Xmf/Highlighter.php
+++ htdocs/xoops_lib/Xmf/Highlighter.php
@@ -18,7 +18,7 @@ namespace Xmf;
  * @package   Xmf
  * @author    Richard Griffith <richard@geekwright.com>
  * @copyright 2011-2013 The XOOPS Project http://sourceforge.net/projects/xoops/
- * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
  * @version   Release: 1.0
  * @link      http://xoops.org
  * @since     1.0
--- htdocs/xoops_lib/Xmf/Metagen.php
+++ htdocs/xoops_lib/Xmf/Metagen.php
@@ -19,7 +19,7 @@ namespace Xmf;
  * @author    Richard Griffith <richard@geekwright.com>
  * @author    trabis <lusopoemas@gmail.com>
  * @copyright 2011-2013 The XOOPS Project http://sourceforge.net/projects/xoops/
- * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
  * @version   Release: 1.0
  * @link      http://xoops.org
  * @since     1.0
