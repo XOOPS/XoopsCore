8259655221ed1c3605725b4f1718c793d5c68898
alain91 <alain091@gmail.com>
2014-03-08 14:32:12 +0100

    add class/xoopseditor test files + one patch

---
+++ UnitTestXoops/class/xoopseditor/textarea/editor_registryTest.php
@@ -0,0 +1,29 @@
+<?php
+require_once(dirname(__FILE__).'/../../../init.php');
+
+global $config;
+$config = null;
+
+/**
+* PHPUnit special settings :
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+class Textarea_Editor_registryTest extends MY_UnitTestCase
+{
+
+    public function test_100()
+    {
+		global $config;
+		
+		ob_start();
+		require_once (XOOPS_ROOT_PATH.'/class/xoopseditor/textarea/editor_registry.php');
+		$x = ob_get_clean();
+		$this->assertTrue(is_array($config));
+		$this->assertTrue(isset($config['class']));
+		$this->assertTrue(isset($config['file']));
+		$this->assertTrue(isset($config['title']));
+		$this->assertTrue(isset($config['order']));
+		$this->assertTrue(isset($config['nohtml']));
+    }
+}
---
+++ UnitTestXoops/class/xoopseditor/tinymce/tiny_mce/plugins/xoops_xlanguage/xoops_xlanguageTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once(dirname(__FILE__).'/../../../../../../init.php');
+
+/**
+* PHPUnit special settings :
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+class Plugins_Xoops_xlanguageTest extends MY_UnitTestCase
+{
+
+    public function test_100()
+    {
+		ob_start();
+		require_once (XOOPS_ROOT_PATH.'/class/xoopseditor/tinymce/tiny_mce/plugins/xoops_xlanguage/xoops_xlanguage.php');
+		$x = ob_get_clean();
+		$this->assertTrue((bool)$x);
+    }
+}
