62b79ba4aefca3e4e72a98757b864e26ea7d3947
geekwright <richard@geekwright.com>
2014-03-19 15:38:54 -0500

    Convert UTF-8 encoding
    Attempted to repair several mangled files as well. Also removed empty or redundant languages. There are issues with the 'german' and 'de_utf8' direcories, as there are substantial differences that still need to be resolved.

--- htdocs/modules/protector/language/arabic/modinfo.php
+++ htdocs/modules/protector/language/arabic/modinfo.php
@@ -3,117 +3,115 @@
 if( defined( 'FOR_XOOPS_LANG_CHECKER' ) ) $mydirname = 'protector' ;
 $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
-if( defined( 'FOR_XOOPS_LANG_CHECKER' ) || ! defined( '_MI_PROTECTOR_LOADED' ) ) {
+if( defined( 'FOR_XOOPS_LANG_CHECKER' ) || ! defined( $constpref.'_LOADED' ) ) {
 
-define('_MI_PROTECTOR_LOADED' , 1 ) ;
+define( $constpref.'_LOADED' , 1 ) ;
 
 // The name of this module
-define("_MI_PROTECTOR_NAME","ÇáÍÇÑÓ áÒæÈÓ");
+define($constpref."_NAME","الحارس لزوبس");
 
 // A brief description of this module
-define("_MI_PROTECTOR_DESC","åÐÇ ÇáÈÑäÇãÌ íæÝÑ áãæÞÚß ÇáÍãÇíÉ ãä ÚãáíÇÊ ÇáÇÎÊÑÇÞ ÇáãÎÊáÝÉ áãæÞÚß");
+define($constpref."_DESC","هذا البرنامج يوفر لموقعك الحماية من عمليات الاختراق المختلفة لموقعك");
 
 // Menu
-define("_MI_PROTECTOR_ADMININDEX","ÇáÑÆíÓíÉ");
-define("_MI_PROTECTOR_ADVISORY","ÊÝÍÕ ÇáÍãÇíÉ");
-define("_MI_PROTECTOR_PREFIXMANAGER","ÇÏÇÑÉ ÌÏæá ÞÇÚÏÉ ÇáÈíÇäÇÊ");
-//define('_MI_PROTECTOR_ADMENU_MYBLOCKSADMIN','ÇáÊÕÇÑíÍ') ;
+define($constpref."_ADMININDEX","الرئيسية");
+define($constpref."_ADVISORY","تفحص الحماية");
+define($constpref."_PREFIXMANAGER","ادارة جدول قاعدة البيانات");
+define($constpref.'_ADMENU_MYBLOCKSADMIN','التصاريح') ;
 
 // Configs
-define('_MI_PROTECTOR_GLOBAL_DISBL','ÊÚØíá ÇáãæÏíá');
-define('_MI_PROTECTOR_GLOBAL_DISBLDSC','ÊÚØíá ÈÑäÇãÌ ÇáÍÇÑÓ ');
+define($constpref.'_GLOBAL_DISBL','تعطيل الموديل');
+define($constpref.'_GLOBAL_DISBLDSC','تعطيل برنامج الحارس ');
 
-define('_MI_PROTECTOR_DEFAULT_LANG','ÇááÛÉ');
-define('_MI_PROTECTOR_DEFAULT_LANGDSC','common.php ÍÏÏ ÇááÛÉ ÇáÊí ÓÊÓÊÚãá ÞÈá ØáÈ ãáÝ  ');
+define($constpref.'_DEFAULT_LANG','اللغة');
+define($constpref.'_DEFAULT_LANGDSC','common.php حدد اللغة التي ستستعمل قبل طلب ملف  ');
 
-define('_MI_PROTECTOR_RELIABLE_IPS','ÇáÇíÈíåÇÊ ÇáÕÏíÞÉ');
-define('_MI_PROTECTOR_RELIABLE_IPSDSC',' |ÖÚ ÇáÇíÈíåÇÊ ÇáÊí ÊÚÊÈÑ ÕÏíÞÉ æíãßä ÇáÇÚÊãÇÏ ÚáíÉ ÇÝÕá ÇáÇíÈíåÇÊ ÈåÐÉ ÇáÇÔÇÑÉ');
+define($constpref.'_RELIABLE_IPS','الايبيهات الصديقة');
+define($constpref.'_RELIABLE_IPSDSC',' |ضع الايبيهات التي تعتبر صديقة ويمكن الاعتماد علية افصل الايبيهات بهذة الاشارة');
 
-define('_MI_PROTECTOR_LOG_LEVEL','ÍÝÙ ÇáÓÌáÇÊ');
-//define('_MI_PROTECTOR_LOG_LEVELDSC','');
+define($constpref.'_LOG_LEVEL','حفظ السجلات');
+define($constpref.'_LOG_LEVELDSC','');
 
-define('_MI_PROTECTOR_BANIP_TIME0','ãÏÉ ÇáãäÚ ááÇíÈí ÇáãÍÖæÑ - ÈÇáËæÇäí)');
+define($constpref.'_BANIP_TIME0','مدة المنع للايبي المحضور - بالثواني)');
 
-define('_MI_PROTECTOR_LOGLEVEL0','ÈÏæä');
-define('_MI_PROTECTOR_LOGLEVEL15','ÚÇÏí');
-define('_MI_PROTECTOR_LOGLEVEL63','ÚÇÏí');
-define('_MI_PROTECTOR_LOGLEVEL255','Çáßá');
+define($constpref.'_LOGLEVEL0','بدون');
+define($constpref.'_LOGLEVEL15','عادي');
+define($constpref.'_LOGLEVEL63','عادي');
+define($constpref.'_LOGLEVEL255','الكل');
 
-define('_MI_PROTECTOR_HIJACK_TOPBIT','ÍãÇíÉ ÇáÇíÈí ÇËäÇÁ ÇáÌáÓå-Çí ÇáÊæÇÌÏ ÈÇáãæÞÚ');
-define('_MI_PROTECTOR_HIJACK_TOPBITDSC','ÇáÍãÇíÉ ááÇíÈí ãä ÓÑÞÉ ÇáßæßíÒ  . ÇÐ ßÇä áß ÇíÈí ËÇÈÊ ÇÎÊÇÑ 32 ÇÐ ßÇä ÛíÑ ËÇÈÊ ÇÎÊÇÑ 24 ßÇÝÊÑÇÖí');
-define('_MI_PROTECTOR_HIJACK_DENYGP','ÇáãÌãæÚÇÊ ÇáÛíÑ ãÓãæÍ ÈäÞáåÇ Çáí äÙÇã ÍãÇíÉ ÇáÌáÓÉ');
-define('_MI_PROTECTOR_HIJACK_DENYGPDSC','ãÇäÚ ÍÞä æÓÑÞÉ ÇáßæßíÒ Ýí ÇáÌáÓÉ:<br />ÇÎÊÇÑ ÇáãÌãæÚÉ ÇáÛíÑ ãÓãæÍ áåÇ ÈÇáÇäÊÞÇá ÊÍÊ äÙÇã ÇáÍãÇíÉ ÇËäÇÁ ÇáÌáÓÉ . ãä ÇáãÞÊÑÍ ÇÎÊíÇÑ ãÌãæÚÉ ÇáÇÏÇÑÉ');
-define('_MI_PROTECTOR_SAN_NULLBYTE','null-bytes ÇáÊÚÞíã áÇæÇãÑ ãä äæÚ');
-define('_MI_PROTECTOR_SAN_NULLBYTEDSC','"\\0" ãä ÇáãÞÊÑÍ ÊÝÚíá åÐÇ ÇáÎíÇÑ áÇä åÐÇ ÇáßæÏ ÛÇáÈÇ ãÇ íÓÊÎÏã Ýí ÚãáíÇÊ ÇáÊÎÑíÈ');
-define('_MI_PROTECTOR_DIE_NULLBYTE','"\\0" ÇáÎÑæÌ Ýí ÍÇáÉ æÌæÏ  ÚãáíÉ ãä äæÚ äíá ÈÇÊÓ');
-define('_MI_PROTECTOR_DIE_NULLBYTEDSC','"\\0" ãä ÇáãÞÊÑÍ ÊÝÚíá åÐÇ ÇáÎíÇÑ áÇä åÐÇ ÇáßæÏ ÛÇáÈÇ ãÇ íÓÊÎÏã Ýí ÚãáíÇÊ ÇáÊÎÑíÈ');
-define('_MI_PROTECTOR_DIE_BADEXT','ÇáÎÑæÌ Ýí ÍÇáÉ ÑÝÚ ãáÝ ÓíÁ');
-define('_MI_PROTECTOR_DIE_BADEXTDSC','ÇÐ ÍÇæá ÇÍÏ ÑÝÚ ãáÝ ÈÕíÛÉ Èí ÇÊÔ Èí  Çæ ÕíÛÉ ÇÎÑí ÛíÑ ãÓãæÍ ÈåÇ<br />ÇÐ ßäÊ Ýí ÇáÛÇáÈ ÊÑÝÚ ãáÝÇÊ ÈÕíÛÉ Èí ÇÊÔ Èí ÝÞã ÇÐ ÈÊÚØíá åÐÇ ÇáÎíÇÑ ');
-define('_MI_PROTECTOR_CONTAMI_ACTION','ãÍÇæáÉ ÊáæíË æÇáÚÈË ÈãÊÛíÑÇÊ ÇáãÌáÉ');
-define('_MI_PROTECTOR_CONTAMI_ACTIONDS','ÇÎÊÇÑ ÇáÚãá Ýí ÍÇáÉ ÇßÊÔÇÝ ãÍÇæáÉ áÊáæíË  æÇáÚÈË ÈãÊÛíÑÇÊ ÇáãÌáÉ ÇáÚÇãÉ<br />ÇáãÞÊÑÍ åæ  ÇÎÊíÇÑ ÕÝÍÉ ÈíÖÇÁ');
-define('_MI_PROTECTOR_ISOCOM_ACTION','ÇáÚãá ÍÇá ÇßÊÔÇÝ ÊÚáíÞ ãáÛæã');
-define('_MI_PROTECTOR_ISOCOM_ACTIONDSC','ãÇäÚ ÇáÍÞ Ýí ÇáÞÇÚÏå:<br />"/*" ÇáÚãá ÍÇá ÇßÊÔÇÝÉ åÐÇ ÇáÑãÒ Ýí ÊÚáíÞ ãÇ<br />ÇáÊÚÞíã íÚäí ÇÖÇÝÉ ÑãÒ ÇáÓáÇÔ ááßæÏ áÊÚØíáÉ - ÇáÚãá ÇáãÞÊÑÍ  åæ ÇÎÊíÇÑ ÊÚÞíã ÇáÇãÑ');
-define('_MI_PROTECTOR_UNION_ACTION','ÇáÚãá ÍÇá ÇßÊÔÇÝ Çí ãä ÇæÇãÑ ÇáÇÊÍÇÏ');
-define('_MI_PROTECTOR_UNION_ACTIONDSC','ãÇäÚ ÇáÍÞä ááÞÇÚÏÉ:<br />ÇÎÊÇÑ ÇáÚãá ÍÇá ÇßÊÔÇÝ Çí ÚãáíÉ ÎÇÑÌíÉ ãä ÚãáíÇÊ ÇáÇÊÍÇÏ æÇáÚãá ÇáãÞÊÑÍ åæ ÊÚÞíã ÇáÇãÑ<br />""union" ÓíÊã ÊÛíÑ ÇáÑãÒ ÈæÖÚ ÏÇÔ  ÈãäÊÕÝ ÇáßáãÉ');
-define('_MI_PROTECTOR_ID_INTVAL','ID ÇæÇãÑ ÇáØáÈ æÇáÌáÈ ãä ÇáÞÇÚÏÉ');
-define('_MI_PROTECTOR_ID_INTVALDSC','"*id" ßá ÇáÇæÇãÑ ÇáÊí ÊäÊåí ÈåÐÇ ÇáÑãÒ<br />ÊÝÚíá ÇáÎíÇÑ íÍãí ãä ÈÚÖ ÚãáíÇÊ ÇáÍÞ<br />åÐÇ ÇáÇÎÊíÇÑ íÓÈÈ ÇÍíÇäÇ ÈÊÚØá ÈÑÇãÌ ÇÎÑí áÐáß ßã ÈÊÚØíáÉ  ÇáÇ ÇÐ ßäÊ ÊÚÑÝ ãÇ ÊÝÚá');
-define('_MI_PROTECTOR_FILE_DOTDOT','Directory TraversalsÇáãäÚ ãä ÚãáíÇÊ ÇáÊäÞá ');
-define('_MI_PROTECTOR_FILE_DOTDOTDSC','ãäÚ ßá ÇáÚãáíÇÊ ÇáÊí ÊÈÏæ  Úáì ÇäåÇ ÊÞæã ÈÇÓÊÚÑÇÖ ÇáãæÞÚ æÇáãáÝÇÊ æÇáÊí ÊÈÍË Úä ËÛÑÇÊ ÈÇáãæÞÚ');
+define($constpref.'_HIJACK_TOPBIT','حماية الايبي اثناء الجلسه-اي التواجد بالموقع');
+define($constpref.'_HIJACK_TOPBITDSC','الحماية للايبي من سرقة الكوكيز  . اذ كان لك ايبي ثابت اختار 32 اذ كان غير ثابت اختار 24 كافتراضي');
+define($constpref.'_HIJACK_DENYGP','المجموعات الغير مسموح بنقلها الي نظام حماية الجلسة');
+define($constpref.'_HIJACK_DENYGPDSC','مانع حقن وسرقة الكوكيز في الجلسة:<br />اختار المجموعة الغير مسموح لها بالانتقال تحت نظام الحماية اثناء الجلسة . من المقترح اختيار مجموعة الادارة');
+define($constpref.'_SAN_NULLBYTE','null-bytes التعقيم لاوامر من نوع');
+define($constpref.'_SAN_NULLBYTEDSC','"\\0" من المقترح تفعيل هذا الخيار لان هذا الكود غالبا ما يستخدم في عمليات التخريب');
+define($constpref.'_DIE_NULLBYTE','"\\0" الخروج في حالة وجود  عملية من نوع نيل باتس');
+define($constpref.'_DIE_NULLBYTEDSC','"\\0" من المقترح تفعيل هذا الخيار لان هذا الكود غالبا ما يستخدم في عمليات التخريب');
+define($constpref.'_DIE_BADEXT','الخروج في حالة رفع ملف سيء');
+define($constpref.'_DIE_BADEXTDSC','اذ حاول احد رفع ملف بصيغة بي اتش بي  او صيغة اخري غير مسموح بها<br />اذ كنت في الغالب ترفع ملفات بصيغة بي اتش بي فقم اذ بتعطيل هذا الخيار ');
+define($constpref.'_CONTAMI_ACTION','محاولة تلويث والعبث بمتغيرات المجلة');
+define($constpref.'_CONTAMI_ACTIONDS','اختار العمل في حالة اكتشاف محاولة لتلويث  والعبث بمتغيرات المجلة العامة<br />المقترح هو  اختيار صفحة بيضاء');
+define($constpref.'_ISOCOM_ACTION','العمل حال اكتشاف تعليق ملغوم');
+define($constpref.'_ISOCOM_ACTIONDSC','مانع الحق في القاعده:<br />"/*" العمل حال اكتشافة هذا الرمز في تعليق ما<br />التعقيم يعني اضافة رمز السلاش للكود لتعطيلة - العمل المقترح  هو اختيار تعقيم الامر');
+define($constpref.'_UNION_ACTION','العمل حال اكتشاف اي من اوامر الاتحاد');
+define($constpref.'_UNION_ACTIONDSC','مانع الحقن للقاعدة:<br />اختار العمل حال اكتشاف اي عملية خارجية من عمليات الاتحاد والعمل المقترح هو تعقيم الامر<br />""union" سيتم تغير الرمز بوضع داش  بمنتصف الكلمة');
+define($constpref.'_ID_INTVAL','ID اوامر الطلب والجلب من القاعدة');
+define($constpref.'_ID_INTVALDSC','"*id" كل الاوامر التي تنتهي بهذا الرمز<br />تفعيل الخيار يحمي من بعض عمليات الحق<br />هذا الاختيار يسبب احيانا بتعطل برامج اخري لذلك كم بتعطيلة  الا اذ كنت تعرف ما تفعل');
+define($constpref.'_FILE_DOTDOT','Directory Traversalsالمنع من عمليات التنقل ');
+define($constpref.'_FILE_DOTDOTDSC','منع كل العمليات التي تبدو  على انها تقوم باستعراض الموقع والملفات والتي تبحث عن ثغرات بالموقع');
 
-define('_MI_PROTECTOR_BF_COUNT','ãÇäÚ ãÍÇæáÉ ÊÓÌíá ÇáÏÎæá ÇáãÊßÑÑå');
-define('_MI_PROTECTOR_BF_COUNTDSC','ÍÏÏ ÚÏÏ ÇáãÑÇÊ ÇáãÓãæÍ ááÚÖæ ÈåÇ áÊÓÌíá ÏÎæáÉ ÈßáãÉ ÓÑ ÛíÑ ÕÍíÍÉ æÈÚÏ ÇáÚÏÏ ÇáãÍÏÏ ÓíÊã ØÑÏÉ');
+define($constpref.'_BF_COUNT','مانع محاولة تسجيل الدخول المتكرره');
+define($constpref.'_BF_COUNTDSC','حدد عدد المرات المسموح للعضو بها لتسجيل دخولة بكلمة سر غير صحيحة وبعد العدد المحدد سيتم طردة');
 
-define('_MI_PROTECTOR_BWLIMIT_COUNT','ÊÍÏíÏ æÖÈØ ÍÌã ÊÈÇÏá ÇáãáÝÇÊ - ÇáÈÇäÏæíÏË');
-define('_MI_PROTECTOR_BWLIMIT_COUNTDSC','mainfile.php ÖÚ ÕÝÑ ááãæÇÞÚ ÇáÊí áÏíåÇ ÞÏÑå ÌíÏå Úáì ÇÓÊíÚÇÈ ÚÏÏ áÇÈÇÓ Èå ãä ÇáÒæÇÑ  æÇí ÑÞã ÇÞá ãä 10 ÓíÊã ÊÌÇåáÉ -ÍÏÏ ÚÏÏ ÇáãÑÇÊ ÇáÊí íÓÊØíÚ ÇáÒÇÆÑ ÝíåÇ ÒíÇÑÉ ãáÝ');
+define($constpref.'_BWLIMIT_COUNT','تحديد وضبط حجم تبادل الملفات - الباندويدث');
+define($constpref.'_BWLIMIT_COUNTDSC','mainfile.php ضع صفر للمواقع التي لديها قدره جيده على استيعاب عدد لاباس به من الزوار  واي رقم اقل من 10 سيتم تجاهلة -حدد عدد المرات التي يستطيع الزائر فيها زيارة ملف');
 
-define('_MI_PROTECTOR_DOS_SKIPMODS',' Crawler ÇáÈÑÇãÌ ÇáÛíÑ ÎÇÖÚÉ áäÙÇã ÇáãÑÇÞÈÉ');
-define('_MI_PROTECTOR_DOS_SKIPMODSDSC','|Þã ÈßÊÇÈÉ ÇÓãÇÁ ÇáãæÏíáÇÊ ÇáÊí ÓíÊã ÇÓÊËäÇÁåÇ ãä ÇáãÑÇÞÈÉ  ÇÝÕá Èíä ÇáÈÑÇãÌ ÈÇáÇÔÇÑå');
+define($constpref.'_DOS_SKIPMODS',' Crawler البرامج الغير خاضعة لنظام المراقبة');
+define($constpref.'_DOS_SKIPMODSDSC','|قم بكتابة اسماء الموديلات التي سيتم استثناءها من المراقبة  افصل بين البرامج بالاشاره');
 
-define('_MI_PROTECTOR_DOS_EXPIRE','ãÑÇÞÈÉ ÇáÖÛØ Úáì ÇáãæÞÚ ÈÇáËæÇäí');
-define('_MI_PROTECTOR_DOS_EXPIREDSC','F5åÐÇ ÇáÇÎÊíÇÑ áãÑÇÞÈÉ ÇáÖÛØ ÇáãÍÏË Úáì ÇáãæÞÚ ãä ÎáÇá ÈÑÇãÌ ÇáÈÍË ãËáÇ Çæ ÍÇá ÇÓÊÎÏÇã äÙÇã ÊÍÏíË Çæ ÑíÝÑíÔ ÇáãæÞÚ ÈÇÓÊÎÏÇã ÇáÇÏÇÉ ');
+define($constpref.'_DOS_EXPIRE','مراقبة الضغط على الموقع بالثواني');
+define($constpref.'_DOS_EXPIREDSC','F5هذا الاختيار لمراقبة الضغط المحدث على الموقع من خلال برامج البحث مثلا او حال استخدام نظام تحديث او ريفريش الموقع باستخدام الاداة ');
 
-define('_MI_PROTECTOR_DOS_F5COUNT',' F5ÚÏÏ ÇáãÑÇÊ áÇÍÊÓÇÈåÇ åÌæã');
-define('_MI_PROTECTOR_DOS_F5COUNTDSC','ááÍãíÇÉ ãä  ÇáÏæÓ æÇÓÊäÒÇÝ ÇáãæÞÚ ÈÇÚÇÏÉ ÊÍãíá ÕÝÍÉ ÇáÈÏÇíÉ ÇßËÑ ãä ãÑå');
-define('_MI_PROTECTOR_DOS_F5ACTION',' F5 ÇáÚãá ÍÇá ÇßÊÔÇÝ åÌæã ãä äæÚ');
+define($constpref.'_DOS_F5COUNT',' F5عدد المرات لاحتسابها هجوم');
+define($constpref.'_DOS_F5COUNTDSC','للحمياة من  الدوس واستنزاف الموقع باعادة تحميل صفحة البداية اكثر من مره');
+define($constpref.'_DOS_F5ACTION',' F5 العمل حال اكتشاف هجوم من نوع');
 
-define('_MI_PROTECTOR_DOS_CRCOUNT','ÚÏÏ ãÑÇÊ ÇáÇÓÊÚÑÇÖ ãä ÞÈá ãÍÑßÇÊ ÇáÈÍË ÞÈá ÇÚÊÈÇÑ ÇáÚãáíÉ åÌæã');
-define('_MI_PROTECTOR_DOS_CRCOUNTDSC','ááãäÚ ãä ßá ÇáÚãáíÇÊ ÇáÊí ÊÞæã ÈãÍÇæáå ÇÓÊÚÑÇÖ ßá ãáÝÇÊ æÑÇæÈØ ãæÞÚß æÇÍÏÇË ÖÛØ ÚáíÉ');
-define('_MI_PROTECTOR_DOS_CRACTION','ÇáÚãá ÍÇá ÇßÊÔÇÝ ÚãáíÇÊ ÇäÔÇÁ ÖÛØ ÚÇáí Úáì ÇáãæÞÚ');
+define($constpref.'_DOS_CRCOUNT','عدد مرات الاستعراض من قبل محركات البحث قبل اعتبار العملية هجوم');
+define($constpref.'_DOS_CRCOUNTDSC','للمنع من كل العمليات التي تقوم بمحاوله استعراض كل ملفات وراوبط موقعك واحداث ضغط علية');
+define($constpref.'_DOS_CRACTION','العمل حال اكتشاف عمليات انشاء ضغط عالي على الموقع');
 
-define('_MI_PROTECTOR_DOS_CRSAFE','ãÍÑßÇÊ ÇáÈÍË ÇáãÓãæÍ áåÇ ');
-define('_MI_PROTECTOR_DOS_CRSAFEDSC','ßá ãÍÑßÇÊ ÇáÈÍË ÇáãÖÇÝÉ ÈÇáÍÞá áä ÊÚÊÈÑ ãÍÑßÇÊ ÈÍË ÓíÆÉ Çæ ÊÍÏË ÖÛØ Úáì ÇáãæÞÚ<br />ãËá<br />eg) /(msnbot|Googlebot|Yahoo! Slurp)/i');
+define($constpref.'_DOS_CRSAFE','محركات البحث المسموح لها ');
+define($constpref.'_DOS_CRSAFEDSC','كل محركات البحث المضافة بالحقل لن تعتبر محركات بحث سيئة او تحدث ضغط على الموقع<br />مثل<br />eg) /(msnbot|Googlebot|Yahoo! Slurp)/i');
 
-define('_MI_PROTECTOR_OPT_NONE','áÇÔíÁ ÝÞØ ÓÌá ÇáÚãáíÉ');
-define('_MI_PROTECTOR_OPT_SAN','ÊÚÞíã ÇáÇãÑ');
-define('_MI_PROTECTOR_OPT_EXIT','ÕÝÍÉ ÈíÖÇÁ');
-define('_MI_PROTECTOR_OPT_BIP','ãäÚ ÇáÇíÈí ááÇÈÏ');
-define('_MI_PROTECTOR_OPT_BIPTIME0','ãäÚ ÇáÇíÈí ãÄÞÊ');
+define($constpref.'_OPT_NONE','لاشيء فقط سجل العملية');
+define($constpref.'_OPT_SAN','تعقيم الامر');
+define($constpref.'_OPT_EXIT','صفحة بيضاء');
+define($constpref.'_OPT_BIP','منع الايبي للابد');
+define($constpref.'_OPT_BIPTIME0','منع الايبي مؤقت');
 
-define('_MI_PROTECTOR_DOSOPT_NONE','áÇÔíÁ ÝÞØ ÓÌá ÇáÚãáíÉ');
-define('_MI_PROTECTOR_DOSOPT_SLEEP','ÚÏã ÇÓÊÌÇÈÉ-äÇÆã');
-define('_MI_PROTECTOR_DOSOPT_EXIT','ÕÝÍÉ ÈíÖÇÁ');
-define('_MI_PROTECTOR_DOSOPT_BIP','ãäÚ ÇáÇíÈí ááÇÈÏ');
-define('_MI_PROTECTOR_DOSOPT_BIPTIME0','ãäÚ ÇáÇíÈí ãÄÞÊ');
-define('_MI_PROTECTOR_DOSOPT_HTA','.htaccess ÇáãäÚ ÈãáÝ');
+define($constpref.'_DOSOPT_NONE','لاشيء فقط سجل العملية');
+define($constpref.'_DOSOPT_SLEEP','عدم استجابة-نائم');
+define($constpref.'_DOSOPT_EXIT','صفحة بيضاء');
+define($constpref.'_DOSOPT_BIP','منع الايبي للابد');
+define($constpref.'_DOSOPT_BIPTIME0','منع الايبي مؤقت');
+define($constpref.'_DOSOPT_HTA','.htaccess المنع بملف');
 
-define('_MI_PROTECTOR_BIP_EXCEPT','ÇáãÌãæÚÉ  ÇáÊí áÇ íÊã ØÑÏåÇ ÇÈÏÇ');
-define('_MI_PROTECTOR_BIP_EXCEPTDSC','ÍÏÏ ÇíÈí ãÚíä   áÍãÇíÊå ãä ÇáØÑÏ ãä ÇáãæÞÚ<br />(ãä ÇáãÞÊÑÍ ÝÞØ ÇíÈí ÇáãÏíÑ');
+define($constpref.'_BIP_EXCEPT','المجموعة  التي لا يتم طردها ابدا');
+define($constpref.'_BIP_EXCEPTDSC','حدد ايبي معين   لحمايته من الطرد من الموقع<br />(من المقترح فقط ايبي المدير');
 
-define('_MI_PROTECTOR_DISABLES','XOOPS ÊÚØíá  ÎÕÇÆÕ ÎØíÑÉ Ýí ãÌáÉ');
+define($constpref.'_DISABLES','XOOPS تعطيل  خصائص خطيرة في مجلة');
 
-define('_MI_PROTECTOR_DBLAYERTRAP','ÊÝÚíá ÇáÞäÇÚ áÖÈØ ÚãáíÇÊ ÇáÍÞä');
-define('_MI_PROTECTOR_DBLAYERTRAPDSC','åÐÇ ÇáÇÎÊíÇÑ íãäÚ ÇáÚÏíÏ ãä ÚãáíÇÊ ÇáÍÞä . æáßä Úáíß ÇáÊÇßÏ ãä ÊÝÍÕ ÇáÍãÇíÉ áãÚÑÝÉ ãÇ Çä ßÇä áÏíß ÇáãÇÓß Çæ ÇáÞäÇÚ');
-define('_MI_PROTECTOR_DBTRAPWOSRV','áÇÊÞã ÇÈÏ ÈÊÝÍÕ ÇáÓíÑÝÑ ãä ãÇäÚ ÇáÍÞä');
-define('_MI_PROTECTOR_DBTRAPWOSRVDSC',' åäÇß ÓíÑÝÑÇÊ áÏíåÇ äÙÇã ãÇäÚ ááÍÞä Ýí ÞÇÚÏÉ ÇáÈíÇäÇÊ - áæ æÇÌåÊ ãÔßáÉ ÈãæÞÚß Þã ÈÊÝÚíá åÐÇ ÇáÇÎÊíÇÑ');
+define($constpref.'_DBLAYERTRAP','تفعيل القناع لضبط عمليات الحقن');
+define($constpref.'_DBLAYERTRAPDSC','هذا الاختيار يمنع العديد من عمليات الحقن . ولكن عليك التاكد من تفحص الحماية لمعرفة ما ان كان لديك الماسك او القناع');
+define($constpref.'_DBTRAPWOSRV','لاتقم ابد بتفحص السيرفر من مانع الحقن');
+define($constpref.'_DBTRAPWOSRVDSC',' هناك سيرفرات لديها نظام مانع للحقن في قاعدة البيانات - لو واجهت مشكلة بموقعك قم بتفعيل هذا الاختيار');
 
-define('_MI_PROTECTOR_BIGUMBRELLA','anti-XSS (BigUmbrella)ÇáÍãÇíÉ ãä ÇáåÌæã ãä äæÚ');
-define('_MI_PROTECTOR_BIGUMBRELLADSC','åÐÇ ÇáäæÚ íÞæã ÇáãåÇÌã ÈÇÑÓÇá ãÍÊæì ãä ÎáÇáÉ íÍÇæá ÓÑÞÉ ÇÑÞÇã ÍÓÇÈÇÊ æÇíãíáÇÊ æÇí ÈíÇäÇÊ ÍÓÇÓÉ ãä ãæÞÚ ÇáÖÍíÉ. ÇáÍÇÑÓ áÇíæÝÑ ÍãÇíÉ ßÇãáÉ áåÐÇ ÇáäæÚ  áÇÎÊáÇÝ ÇäæÇÚ ÇáåÌæã ');
+define($constpref.'_BIGUMBRELLA','anti-XSS (BigUmbrella)الحماية من الهجوم من نوع');
+define($constpref.'_BIGUMBRELLADSC','هذا النوع يقوم المهاجم بارسال محتوى من خلالة يحاول سرقة ارقام حسابات وايميلات واي بيانات حساسة من موقع الضحية. الحارس لايوفر حماية كاملة لهذا النوع  لاختلاف انواع الهجوم ');
 
-define('_MI_PROTECTOR_SPAMURI4U','ãÇäÚ ÇáÓÈÇã ááÇÚÖÇÁ');
-define('_MI_PROTECTOR_SPAMURI4UDSC','Çí ãæÖæÚ Çæ ÊÚáíÞ ãä ÞÈá ÇáÇÚÖÇÁ íÍÊæí åÐÇ ÇáÚÏÏ ãä ÇáÑæÇÈØ ÓíÚÊÈÑ ÓÈÇã æÖÚß ÕÝÑ íÚäí ÊÚØíá ÇáÇÎÊíÇÑ');
-define('_MI_PROTECTOR_SPAMURI4G','ãÇäÚ ÇáÓÈÇã ááÒæÇÑ');
-define('_MI_PROTECTOR_SPAMURI4GDSC','Çí ãæÖæÚ Çæ ÊÚáíÞ íÍÊæí åÐÇ ÇáÚÏÏ ãä ÇáÑæÇÈØ ÓíÚÊÈÑ ÓÈÇã æÖÚ ÕÝÑ íÚäí ÊÚØíáß ááÇÎÊíÇÑ');
+define($constpref.'_SPAMURI4U','مانع السبام للاعضاء');
+define($constpref.'_SPAMURI4UDSC','اي موضوع او تعليق من قبل الاعضاء يحتوي هذا العدد من الروابط سيعتبر سبام وضعك صفر يعني تعطيل الاختيار');
+define($constpref.'_SPAMURI4G','مانع السبام للزوار');
+define($constpref.'_SPAMURI4GDSC','اي موضوع او تعليق يحتوي هذا العدد من الروابط سيعتبر سبام وضع صفر يعني تعطيلك للاختيار');
 
 }
-
-?>
