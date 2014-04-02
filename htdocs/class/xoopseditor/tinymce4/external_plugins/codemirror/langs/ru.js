3249682cfec934bf5a07b4f7d6c28fd84dd245a3
alain01 <alainternet01@free.fr>
2014-04-02 14:43:54 +0200

    Add TinyMCE editor V4 with many others plugins

---
+++ htdocs/class/xoopseditor/tinymce4/external_plugins/codemirror/langs/ru.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('ru',{
+	'HTML source code': 'Исходный код HTML',
+	'Start search': 'Приступить к поиску',
+	'Find next': 'Искать далее',
+	'Find previous': 'Найти предыдущий',
+	'Replace': 'Заменить',
+	'Replace all': 'Заменить все'
+});
---
+++ htdocs/class/xoopseditor/tinymce4/include/xoopsemotions.php
@@ -0,0 +1,4 @@
+<?php/** *  TinyMCE adapter for XOOPS * * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license * @package         class * @subpackage      editor * @since           2.3.0 * @author          Laurent JEN <dugris@frxoops.org> * @version         $Id: xoopsemotions.php 8066 2011-11-06 05:09:33Z beckmi $ */
+if (!defined("XOOPS_ROOT_PATH")) { die("XOOPS root path not defined"); }
+include_once XOOPS_ROOT_PATH . "/modules/system/constants.php";// Add your code here to check acces by groupsreturn true;
+?>
