9dbcd59c7167a3b334eaf0624fe3141f21b47bdb
geekwright <richard@geekwright.com>
2014-06-08 12:30:46 -0500

    Several overlapping changes
    PSR4 autoloader support, event triggered so modules can register by namespace similar to existing classmap support.
    MediaUploader now namespaced
    Extended mimetype support
    Modernize domain recognition
    Improved cachabilty for browse.php results

--- UnitTestXoops/xoopsLib/XoopsTest.php
+++ UnitTestXoops/xoopsLib/XoopsTest.php
@@ -640,13 +640,23 @@ class XoopsTest extends MY_UnitTestCase
         $this->assertTrue(is_string($value));
     }
 
-    public function test_5400()
+    public function test_simpleFooter()
     {
-        $instance = Xoops::getInstance();
+        $xoops = Xoops::getInstance();
 
-        //$instance->simpleFooter();
-        //$this->assertTrue(is_string($value));
-        $this->markTestSkipped('');
+        $output = null;
+
+        $callback =
+            function ($buffer, $phase) use (&$output) {
+                $output = $buffer;
+                return '';
+            };
+
+        ob_start($callback);
+        $xoops->simpleFooter();
+        $this->assertTrue(is_string($output), $output);
+        $this->assertSame(substr($output,-7), '</html>', $output);
+        //$this->markTestSkipped('');
     }
 
     public function test_5500()
@@ -1041,12 +1051,42 @@ class XoopsTest extends MY_UnitTestCase
         $instance->disableModuleCache();
     }
 
-    public function test_8000()
+    public function test_getBaseDomain()
     {
-        $instance = Xoops::getInstance();
+        $xoops = Xoops::getInstance();
 
-        $value = $instance->getBaseDomain('http::/localhost/tmp');
-        $this->assertSame('', $value);
+        // test cases url, expected base return (default), expected full return ($includeSubdomain==true)
+        $urls = array(
+            array('url' => 'http://user:pass@www.pref.okinawa.jp:8080/path/to/page.html?query=string#fragment', 'base' => 'pref.okinawa.jp', 'full' => 'www.pref.okinawa.jp',),
+            array('url' => 'http://localhost/test/domain/', 'base' => 'localhost', 'full' => 'localhost',),
+            array('url' => 'https://192.168.1.251/xoops/', 'base' => '192.168.1.251', 'full' => '192.168.1.251',),
+            array('url' => 'http://WWW.PREF.OKINAWA.JP/', 'base' => 'pref.okinawa.jp', 'full' => 'www.pref.okinawa.jp',),
+            array('url' => 'http://www.example.com/', 'base' => 'example.com', 'full' => 'www.example.com',),
+            array('url' => 'http://fred.users.example.com/', 'base' => 'example.com', 'full' => 'fred.users.example.com',),
+            array('url' => 'ftp://example.com', 'base' => 'example.com', 'full' => 'example.com',),
+            array('url' => 'https://www.scottwills.co.uk/', 'base' => 'scottwills.co.uk', 'full' => 'www.scottwills.co.uk',),
+            array('url' => 'http://co.uk/', 'base' => null, 'full' => null,),
+            array('url' => 'http://xoops.consulting', 'base' => 'xoops.consulting', 'full' => 'xoops.consulting',),
+            array('url' => 'okinawa.jp', 'base' => null, 'full' => null,),
+            array('url' => 'http://இலங்கை.museum', 'base' => 'இலங்கை.museum', 'full' => 'இலங்கை.museum',),
+            array('url' => 'http://россия.net', 'base' => 'россия.net', 'full' => 'россия.net',),
+            array('url' => 'http://私の団体も.jp/', 'base' => '私の団体も.jp', 'full' => '私の団体も.jp',),
+            array('url' => 'https://中国化工集团公司.公司:8080/test', 'base' => '中国化工集团公司.公司', 'full' => '中国化工集团公司.公司',),
+            array('url' => '公司', 'base' => null, 'full' => null,),
+            array('url' => 'http://321.4.1.512/', 'base' => '1.512', 'full' => '321.4.1.512',),
+        // ipv6 examples from http://www.ietf.org/rfc/rfc2732.txt
+            array('url' => 'https://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html', 'base' => 'fedc:ba98:7654:3210:fedc:ba98:7654:3210', 'full' => 'fedc:ba98:7654:3210:fedc:ba98:7654:3210',),
+            array('url' => 'http://[1080:0:0:0:8:800:200C:417A]/index.html', 'base' => '1080:0:0:0:8:800:200c:417a', 'full' => '1080:0:0:0:8:800:200c:417a',),
+            array('url' => 'http://[3ffe:2a00:100:7031::1]', 'base' => '3ffe:2a00:100:7031::1', 'full' => '3ffe:2a00:100:7031::1',),
+            array('url' => 'http://[1080::8:800:200C:417A]/foo', 'base' => '1080::8:800:200c:417a', 'full' => '1080::8:800:200c:417a',),
+            array('url' => 'http://[::192.9.5.5]/ipng', 'base' => '::192.9.5.5', 'full' => '::192.9.5.5',),
+            array('url' => 'http://[::FFFF:129.144.52.38]:80/index.html', 'base' => '::ffff:129.144.52.38', 'full' => '::ffff:129.144.52.38',),
+            array('url' => 'http://[2010:836B:4179::836B:4179]', 'base' => '2010:836b:4179::836b:4179', 'full' => '2010:836b:4179::836b:4179',),
+        );
+        foreach ($urls as $url) {
+            $this->assertSame($xoops->getBaseDomain($url['url']), $url['base'], $url['url']);
+            $this->assertSame($xoops->getBaseDomain($url['url'], true), $url['full'], $url['url']);
+        }
     }
 
     public function test_8100()
---
+++ htdocs/xoops_lib/Xoops/Core/Psr4ClassLoader.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Copyright (c) 2013 PHP Framework Interop Group
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Xoops\Core;
+
+/**
+ * A Class Loader that implements the technical interoperability
+ * standards for PHP 5.3 namespaces and class names set forth in PSR-4.
+ * This general-purpose implementation includes the optional functionality
+ * of allowing multiple base directories for a single namespace prefix.
+ *
+ * From the PHP Framework Interop Group
+ *
+ * Examples
+ *
+ * Given a foo-bar package of classes in the file system at the following
+ * paths ...
+ *
+ *     /path/to/packages/foo-bar/
+ *         src/
+ *             Baz.php             # Foo\Bar\Baz
+ *             Qux/
+ *                 Quux.php        # Foo\Bar\Qux\Quux
+ *         tests/
+ *             BazTest.php         # Foo\Bar\BazTest
+ *             Qux/
+ *                 QuuxTest.php    # Foo\Bar\Qux\QuuxTest
+ *
+ * ... add the path to the class files for the \Foo\Bar\ namespace prefix
+ * as follows:
+ *
+ * @code
+ *      <?php
+ *      // instantiate the loader
+ *      $loader = new \Xoops\Core\Psr4ClassLoader;
+ *
+ *      // register the autoloader
+ *      $loader->register();
+ *
+ *      // register the base directories for the namespace prefix
+ *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/src');
+ *      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/tests');
+ * @endcode
+ *
+ * The following line would cause the autoloader to attempt to load the
+ * \Foo\Bar\Qux\Quux class from /path/to/packages/foo-bar/src/Qux/Quux.php:
+ *
+ * @code
+ *      <?php
+ *      new \Foo\Bar\Qux\Quux;
+ * @endcode
+ *
+ * The following line would cause the autoloader to attempt to load the
+ * \Foo\Bar\Qux\QuuxTest class from /path/to/packages/foo-bar/tests/Qux/QuuxTest.php:
+ *
+ * @code
+ *      <?php
+ *      new \Foo\Bar\Qux\QuuxTest;
+ * @endcode
+ *
+ * @category  Xoops\Core\Psr4ClassLoader
+ * @package   Xoops
+ * @author    https://github.com/php-fig/fig-standards/
+ * @author    Richard Griffith <richard@geekwright.com>
+ * @copyright 2013 PHP Framework Interop Group
+ * @copyright 2013-2014 The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license   MIT (see above)
+ * @link      https://github.com/php-fig/fig-standards/wiki/PSR-4-Example-Implementations
+ * @see       http://www.php-fig.org/
+ */
+class Psr4ClassLoader
+{
+    /**
+     * An associative array where the key is a namespace prefix and the value
+     * is an array of base directories for classes in that namespace.
+     *
+     * @var array
+     */
+    protected $prefixes = array();
+
+    /**
+     * addLoader sets all basic options and registers the autoloader
+     *
+     * @param type  $namespace namespace
+     * @param mixed $path      path(s) to the namespace's directories
+     *                         Can be string - only one directory
+     *                         or array of strings - multiple directories
+     *
+     * @return SplClassLoader
+     */
+    public static function addLoader($namespace, $path)
+    {
+        $loaderClass = get_called_class();
+        $loader = new $loaderClass($namespace, $path);
+        if (is_array($path)) {
+            foreach ($path as $pathdir) {
+                $loader->addNamespace($namespace, $pathdir);
+            }
+        } else {
+            $loader->addNamespace($namespace, $path);
+        }
+        $loader->register();
+        return $loader;
+    }
+
+    /**
+     * Register loader with SPL autoloader stack.
+     *
+     * @return null
+     */
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    /**
+     * Adds a base directory for a namespace prefix.
+     *
+     * @param string $prefix   The namespace prefix.
+     * @param string $base_dir Base directory for class files in namespace.
+     * @param bool   $prepend  If true, prepend the base directory to the
+     *                         stack instead of appending it; this causes
+     *                         it to be searched first rather than last.
+     *
+     * @return null
+     */
+    public function addNamespace($prefix, $base_dir, $prepend = false)
+    {
+        // normalize namespace prefix
+        $prefix = trim($prefix, '\\') . '\\';
+
+        // normalize the base directory with a trailing separator
+        $base_dir = rtrim($base_dir, '/') . DIRECTORY_SEPARATOR;
+        $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . '/';
+
+        // initialize the namespace prefix array
+        if (isset($this->prefixes[$prefix]) === false) {
+            $this->prefixes[$prefix] = array();
+        }
+
+        // retain the base directory for the namespace prefix
+        if ($prepend) {
+            array_unshift($this->prefixes[$prefix], $base_dir);
+        } else {
+            array_push($this->prefixes[$prefix], $base_dir);
+        }
+    }
+
+    /**
+     * Loads the class file for a given class name.
+     *
+     * @param string $class The fully-qualified class name.
+     *
+     * @return mixed  The mapped file name on success, or boolean false on
+     * failure.
+     */
+    public function loadClass($class)
+    {
+        // the current namespace prefix
+        $prefix = $class;
+
+        // work backwards through the namespace names of the fully-qualified
+        // class name to find a mapped file name
+        while (false !== $pos = strrpos($prefix, '\\')) {
+
+            // retain the trailing namespace separator in the prefix
+            $prefix = substr($class, 0, $pos + 1);
+
+            // the rest is the relative class name
+            $relative_class = substr($class, $pos + 1);
+
+            // try to load a mapped file for the prefix and relative class
+            $mapped_file = $this->loadMappedFile($prefix, $relative_class);
+            if ($mapped_file) {
+                return $mapped_file;
+            }
+
+            // remove the trailing namespace separator for the next iteration
+            // of strrpos()
+            $prefix = rtrim($prefix, '\\');
+        }
+
+        // never found a mapped file
+        return false;
+    }
+
+    /**
+     * Load the mapped file for a namespace prefix and relative class.
+     *
+     * @param string $prefix         The namespace prefix.
+     * @param string $relative_class The relative class name.
+     *
+     * @return mixed  Boolean false if no mapped file can be loaded, or the
+     * name of the mapped file that was loaded.
+     */
+    protected function loadMappedFile($prefix, $relative_class)
+    {
+        // are there any base directories for this namespace prefix?
+        if (isset($this->prefixes[$prefix]) === false) {
+            return false;
+        }
+
+        // look through base directories for this namespace prefix
+        foreach ($this->prefixes[$prefix] as $base_dir) {
+
+            // replace the namespace prefix with the base directory,
+            // replace namespace separators with directory separators
+            // in the relative class name, append with .php
+            $file = $base_dir
+                  . str_replace('\\', DIRECTORY_SEPARATOR, $relative_class)
+                  . '.php';
+            $file = $base_dir
+                  . str_replace('\\', '/', $relative_class)
+                  . '.php';
+
+            // if the mapped file exists, require it
+            if ($this->requireFile($file)) {
+                // yes, we're done
+                return $file;
+            }
+        }
+
+        // never found it
+        return false;
+    }
+
+    /**
+     * If a file exists, require it from the file system.
+     *
+     * @param string $file The file to require.
+     *
+     * @return bool   True if the file exists, false if not.
+     */
+    protected function requireFile($file)
+    {
+        if (file_exists($file)) {
+            require $file;
+
+            return true;
+        }
+
+        return false;
+    }
+}
