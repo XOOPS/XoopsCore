abe1aafb6367a846879141b32a0a233b4d31062b
alain91 <alain091@gmail.com>
2014-07-19 16:08:45 +0200

    complete Core tests

--- UnitTestXoops/xoopsLib/Xoops/Core/Database/Logging/XoopsDebugStackTest.php
+++ UnitTestXoops/xoopsLib/Xoops/Core/Database/Logging/XoopsDebugStackTest.php
@@ -8,18 +8,21 @@ require_once(dirname(__FILE__).'/../../../../../init_mini.php');
 */
 class XoopsDebugStackTest extends MY_UnitTestCase
 {
-    protected $myclass = 'Xoops\Core\Database\Logging\XoopsDebugStack';
+    protected $myClass = 'Xoops\Core\Database\Logging\XoopsDebugStack';
 	
     public function test___construct()
 	{
-		$instance = new $this->myclass();
-		$this->assertInstanceOf($this->myclass, $instance);
+		$instance = new $this->myClass();
+		$this->assertInstanceOf($this->myClass, $instance);
 		$this->assertInstanceOf('Doctrine\DBAL\Logging\DebugStack', $instance);
     }
 	
     public function test_stopQuery()
 	{
-		$this->markTestIncomplete();
+		$instance = new $this->myClass();
+		$this->assertInstanceOf($this->myClass, $instance);
+		
+		$instance->stopQuery();
     }
 
 }
