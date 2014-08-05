f4d9923dfb903c98d962c1a53867f2ed1f29211f
geekwright <richard@geekwright.com>
2014-08-04 23:33:32 -0500

    Fixes for some PHP 5.3.x issues, possible fix for #173

--- htdocs/install/page_configsave.php
+++ htdocs/install/page_configsave.php
@@ -127,7 +127,11 @@ if (isset($composer->extra) && is_object($composer->extra)) {
 } else {
     $composer->extra['xoops_modules_path'] = $xoops_modules_path;
 }
-file_put_contents($composer_path, json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+$jsonEncodeOpts = 0;
+if (defined('JSON_PRETTY_PRINT') && defined('JSON_UNESCAPED_SLASHES')) {
+    $jsonEncodeOpts = $jsonEndodeOpts | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES;
+}
+file_put_contents($composer_path, json_encode($composer, $jsonEncodeOpts));
 
 $settings['authorized'] = false;
 if (empty($error)) {
