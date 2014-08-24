5d4a36fbf7ac4164eb9d04f5c2aacc334d780af9
geekwright <richard@geekwright.com>
2014-08-24 16:47:10 -0500

    Restructure jquery/ui directory, add i18n

--- htdocs/themes/default/theme_onload.php
+++ htdocs/themes/default/theme_onload.php
@@ -4,7 +4,7 @@
  */
 $xoops = Xoops::getInstance();
 // replace the jquery ui theme config option with theme based asset definition
-$xoops->theme()->setNamedAsset('jqueryuicss', 'media/jquery/ui/smoothness/jquery-ui.css');
+$xoops->theme()->setNamedAsset('jqueryuicss', 'media/jquery/ui/themes/smoothness/jquery-ui.css');
 $xoops->theme()->setNamedAsset('bootstrap', 'media/bootstrap/js/bootstrap.min.js');
 $xoops->theme()->addBaseScriptAssets(array('@jquery', '@bootstrap'));
 
