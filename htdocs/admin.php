9b9a32ab54f6a2d00990eac9768797b46aec0ca3
geekwright <richard@geekwright.com>
2015-02-28 23:05:04 -0600

    Fix a couple of PHP version compatibility issues

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -75,7 +75,8 @@ if ($xoops->getConfig('admin_warnings_enable')) {
     $xoops->tpl()->assign('error_msg', $error_msg);
 }
 
-if (!empty(Request::getString('xoopsorgnews', null, 'GET'))) {
+$xoopsorgnews = Request::getString('xoopsorgnews', null, 'GET');
+if (!empty($xoopsorgnews)) {
     // Multiple feeds
     $myts = MyTextSanitizer::getInstance();
     $rssurl = array();
