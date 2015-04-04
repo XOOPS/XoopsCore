e135749ade5436cb2b6877ccc696f6ec89fde1e8
alain91 <alain091@gmail.com>
2015-04-04 22:46:57 +0200

    Update file & folder tests

--- tests/unit/class/file/xoopsfileTest.php
+++ tests/unit/class/file/xoopsfileTest.php
@@ -8,11 +8,47 @@ require_once(dirname(__FILE__).'/../../init_mini.php');
 */
 class XoopsFileTest extends \PHPUnit_Framework_TestCase
 {
-    protected $myclass = 'XoopsFile';
+    protected $myClass = 'XoopsFile';
 
-    public function test___construct()
+    public function test_getHandler()
 	{
-		$instance = new $this->myclass();
-		$this->assertInstanceOf($this->myclass, $instance);
+		$class = $this->myClass;
+		$instance = $class::getHandler();
+		$this->assertInstanceOf('XoopsFileHandler', $instance);
+		
+		unset($instance);
+		$instance = $class::getHandler('folder');
+		$this->assertInstanceOf('XoopsFolderHandler', $instance);
+		
+		unset($instance);
+		$instance = $class::getHandler('NotValid');
+		$this->assertFalse($instance);
+		
+		unset($instance);
+		$file = dirname(__FILE__).DS.'dummy.txt';
+		$instance = $class::getHandler('file',$file);
+		$this->assertInstanceOf('XoopsFileHandler', $instance);
+		$this->assertFalse(file_exists($file));
+		$this->assertSame(basename($file),$instance->name);
+		$this->assertSame(dirname($file),$instance->folder->path);
+		
+		unset($instance);
+		$file = dirname(__FILE__).DS.'dummy.txt';
+		$instance = $class::getHandler('file',$file,true);
+		$this->assertInstanceOf('XoopsFileHandler', $instance);
+		$this->assertTrue(file_exists($file));
+		$this->assertSame(basename($file),$instance->name);
+		$this->assertSame(dirname($file),$instance->folder->path);
+		$this->assertTrue(@unlink($file));
+		
+		unset($instance);
+		$file = dirname(__FILE__).DS.'dummy.txt';
+		$mode = '0731';
+		$instance = $class::getHandler('file',$file,false,$mode);
+		$this->assertInstanceOf('XoopsFileHandler', $instance);
+		$this->assertFalse(file_exists($file));
+		$this->assertSame(basename($file),$instance->name);
+		$this->assertSame(dirname($file),$instance->folder->path);
+		$this->assertSame(intval($mode,8),$instance->folder->mode);
     }
 }
