0b930f336bfcc66bc0c56ec0ce87699a006bb12b
Richard Griffith <richard@geekwright.com>
2015-04-05 18:53:00 -0500

    Revert "Revert "A few fixes for issues reported by scrutinizer""

--- htdocs/kernel/privmessage.php
+++ htdocs/kernel/privmessage.php
@@ -163,7 +163,7 @@ class XoopsPrivmessageHandler extends XoopsPersistableObjectHandler
     public function setRead(XoopsPrivmessage &$pm)
     {
         $qb = $this->db2->createXoopsQueryBuilder()
-            ->updatePrefix('priv_msgs', 'pm')
+            ->update($this->table, 'pm')
             ->set('pm.read_msg', ':readmsg')
             ->where('pm.msg_id = :msgid')
             ->setParameter(':readmsg', 1, \PDO::PARAM_INT)
