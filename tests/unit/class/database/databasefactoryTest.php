b80acb81f83f1a4ceddfd972aeb5baae043bfbfb
geekwright <richard@geekwright.com>
2015-09-13 20:32:39 -0500

    Cannot always connect to db anonymously

--- tests/unit/class/database/databasefactoryTest.php
+++ tests/unit/class/database/databasefactoryTest.php
@@ -11,23 +11,22 @@ class XoopsDatabaseFactoryTest extends \PHPUnit_Framework_TestCase
     protected $myClass = 'XoopsDatabaseFactory';
 
     public function setUp()
-	{
+    {
     }
 
     public function test_getDatabaseConnection()
-	{
-		$class = $this->myClass;
-		$instance = $class::getDatabaseConnection();
-		$this->assertStringStartsWith('XoopsMySQLDatabase', get_class($instance));
-		
-		$instance2 = $class::getDatabaseConnection();
-		$this->assertSame($instance, $instance2);
-		$this->assertInstanceOf('\\Xoops\\Core\\Database\\Connection', $instance->conn);
-		$driver = $instance->conn->getDriver();
-		$driver_conn = $driver->connect(array());
-		$this->assertInstanceOf('\\Doctrine\\DBAL\\Driver\\PDOConnection', $driver_conn);
+    {
+        $class = $this->myClass;
+        $instance = $class::getDatabaseConnection();
+        $this->assertStringStartsWith('XoopsMySQLDatabase', get_class($instance));
+
+        $instance2 = $class::getDatabaseConnection();
+        $this->assertSame($instance, $instance2);
+        $this->assertInstanceOf('\\Xoops\\Core\\Database\\Connection', $instance->conn);
+        $driver = $instance->conn->getDriver();
+        //$driver_conn = $driver->connect(array()); // not always possible
+        $this->assertInstanceOf('\\Doctrine\\DBAL\\Driver', $driver);
         $this->assertSame(\XoopsBaseConfig::get('db-prefix').'_test', $instance->prefix('test'));
         $this->assertSame(\XoopsBaseConfig::get('db-prefix'), $instance->prefix());
     }
-
 }
