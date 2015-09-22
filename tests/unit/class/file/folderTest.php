fb14014b260debf2f10b1e68cb15a987ada7cfb6
alain91 <alain091@gmail.com>
2015-09-22 19:32:46 +0200

    patch Class\Folder + test

--- tests/unit/class/file/folderTest.php
+++ tests/unit/class/file/folderTest.php
@@ -64,6 +64,7 @@ class XoopsFolderHandlerTest extends \PHPUnit_Framework_TestCase
 		$this->assertSame($dir,$pwd);
 
 		$dir = \XoopsBaseConfig::get('var-path') . '/caches/xoops_cache';
+        $dir = str_replace('/',DIRECTORY_SEPARATOR, str_replace('\\','/',$dir));
 		$instance = new $this->myClass('', false, false);
 		$pwd = $instance->pwd();
 		$this->assertSame($dir,$pwd);
@@ -80,11 +81,16 @@ class XoopsFolderHandlerTest extends \PHPUnit_Framework_TestCase
 		$cd = $instance->cd($dir);
 		$this->assertSame($dir,$cd);
 
-		$dir = dirname(__FILE__).'NOT_EXISTING_FOLDER_TEST';
-		$instance = new $this->myClass($dir, false, false);
-		$this->assertFalse(file_exists($dir));
-		$cd = $instance->cd($dir);
-		$this->assertFalse($cd);
+		$dir = dirname(__FILE__);
+        $instance = new $this->myClass($dir, false, false);
+        $this->assertTrue(file_exists($dir));
+        try {
+            $cd = $instance->cd($dir);
+            $this->assertFalse($cd);
+        }
+        catch (Exception $ex) {
+            $this->assertTrue(true);
+        }
     }
 
     /**
@@ -134,8 +140,7 @@ class XoopsFolderHandlerTest extends \PHPUnit_Framework_TestCase
 		$value = $instance->find('.*Test.php', true);
         $this->assertTrue(is_array($value));
 
-		$instance = new $this->myClass('NOT_FOUND_DIR', false, false);
-		$value = $instance->find('.*Test.php');
+		$value = $instance->find('.*TestDoesntExists.php');
         $this->assertSame(array(), $value);
     }
 
