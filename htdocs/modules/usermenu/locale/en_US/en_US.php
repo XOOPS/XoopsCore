35b28edcea32066646ddb0abac6cdcb5c837156c
trabisdementia <lusopoemas@gmail.com>
2017-03-12 19:12:54 +0000

    Adding usermenu module

---
+++ htdocs/modules/usermenu/locale/en_US/en_US.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * usermenu module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package         usermenu
+ * @since           2.6.0
+ * @author          trabis <lusopoemas@gmail.com>
+ * @version         $Id$
+ */
+class UsermenuLocaleEn_US
+{
+    // Module
+    const MODULE_NAME = "User Menu";
+    const MODULE_DESC = "Provides an extensible User Menu block";
+    const DONATION_DESC = "Donation for module User Menu";
+
+    const BLOCK_NAME = "User menu";
+    const BLOCK_DESC = "Shows an User Menu Block";
+}
