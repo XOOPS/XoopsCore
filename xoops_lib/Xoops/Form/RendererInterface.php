02f1613af5904155df6a1600310efc29c0f9d473
Grégory Mage <gregory.mage@gmail.com>
2019-04-28 22:26:27 +0200

    add namespace

--- xoops_lib/Xoops/Form/RendererInterface.php
+++ xoops_lib/Xoops/Form/RendererInterface.php
@@ -7,7 +7,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
+namespace Xoops\Form;
 /**
  * Define Renderer interface for forms
  *
