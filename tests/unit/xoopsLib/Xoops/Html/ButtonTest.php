81dcbaca7acdaf6db1b0c66ba2d1cc2d12d3dd5e
Richard Griffith <richard@geekwright.com>
2019-05-03 11:43:14 -0500

    Add Xoops/Html/Button class

---
+++ tests/unit/xoopsLib/Xoops/Html/ButtonTest.php
@@ -0,0 +1,50 @@
+<?php
+require_once(__DIR__.'/../../../init_new.php');
+
+use Xoops\Html\Img;
+
+class ButtonTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var Img
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new Button([
+            'type'  => 'submit',
+            'class' => 'button1',
+            'value' => 'sometext'
+        ]);
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    public function test__construct()
+    {
+        $object = new Button(array('type' => 'button'));
+        $this->assertInstanceOf('\Xoops\Html\Attributes', $object);
+        $this->assertEquals('button', $object->get('type'));
+    }
+
+    public function testRender()
+    {
+        $output = $this->object->render();
+        $this->assertStringStartsWith('<button ', $output);
+        $this->assertStringEndsWith('</button>', $output);
+        $this->assertGreaterThanOrEqual(7, strpos($output, 'type="submit"'));
+        $this->assertGreaterThanOrEqual(7, strpos($output, 'class="button1"'));
+        $this->assertGreaterThanOrEqual(7, strpos($output, '>sometext<'));
+    }
+}
---
+++ xoops_lib/Xoops/Html/Button.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+
+namespace Xoops\Html;
+
+/**
+ * Img - Render an html img tag
+ *
+ * @category  Xoops\Html\Img
+ * @package   Xoops\Html
+ * @author    Richard Griffith <richard@geekwright.com>
+ * @copyright 2019 XOOPS Project (https://xoops.org)
+ * @license   GNU GPL 2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
+ */
+class Button extends Attributes
+{
+    /**
+     * __construct
+     *
+     * @param array $attributes array of attribute name => value pairs for button tag.
+     *                          Pass the button text as 'value' attribute.
+     */
+    public function __construct($attributes = array())
+    {
+        parent::__construct($attributes);
+    }
+
+    /**
+     * render a button
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $this->suppressRender(['value']);
+        $tag = '<button ' . $this->renderAttributeString() . '>'
+            . $this->get('value', '')
+            . '</button>';
+
+        return $tag;
+    }
+}
