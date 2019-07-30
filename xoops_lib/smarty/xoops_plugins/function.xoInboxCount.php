5d9ce51286849e40d65054dde4988d197675bd37
Richard Griffith <richard@geekwright.com>
2019-07-29 21:10:03 -0500

    Match 2.5.10 functionality
    - add total parameter
    - use Smarty 3 signatures
    - code standard cleanup

--- xoops_lib/smarty/xoops_plugins/function.xoInboxCount.php
+++ xoops_lib/smarty/xoops_plugins/function.xoInboxCount.php
@@ -1,29 +1,68 @@
 <?php
 
-function smarty_function_xoInboxCount($params, &$smarty)
+use Xoops\Core\Kernel\Criteria;
+use Xoops\Core\Kernel\CriteriaCompo;
+
+/**
+ * xoInboxCount lets templates access private message inbox statistics for the current user
+ *
+ * Example: {xoInboxCount assign='unread_count' total='inbox_total'}
+ *
+ * Both assign and total parameters are optional. If neither is specified the unread count is displayed.
+ * - assign = variable name to assign with the current unread message count
+ * - total  = variable name to assign with the current inbox total
+ *
+ * @param array                    $params parameters
+ * @param Smarty_Internal_Template $smarty template
+ * @return null
+ */
+function smarty_function_xoInboxCount($params, Smarty_Internal_Template $smarty)
 {
+    $assign = 'assign';
+    $total = 'total';
+    $inbox_count = 'xoops_inbox_count';
+    $inbox_total = 'xoops_inbox_total';
+    $inbox_expire = 'xoops_inbox_count_expire';
+
     $xoops = Xoops::getInstance();
+    $session = $xoops->session();
 
     if (!$xoops->isUser()) {
         return;
     }
     $time = time();
-    if (isset($_SESSION['xoops_inbox_count']) && @$_SESSION['xoops_inbox_count_expire'] > $time) {
-        $count = (int)($_SESSION['xoops_inbox_count']);
+    if ($session->has($inbox_count)
+        && $session->has($inbox_expire)
+        && $session->get($inbox_expire) > $time
+    ) {
+        $totals[$assign] = (int)$session->get($inbox_count);
+        $totals[$total] = (int)$session->get($inbox_total);
     } else {
-        $pm_handler = $xoops->getHandlerPrivateMessage();
+        $pmHandler = $xoops->getHandlerPrivateMessage();
+        $eventArgs = [$pmHandler];
 
-        $xoops->events()->triggerEvent('core.class.smarty.xoops_plugins.xoinboxcount', array($pm_handler));
+        $xoops->events()->triggerEvent('core.class.smarty.xoops_plugins.xoinboxcount', $eventArgs);
+        $pmHandler = $eventArgs[0];
 
-        $criteria = new CriteriaCompo(new Criteria('read_msg', 0));
-        $criteria->add(new Criteria('to_userid', $xoops->user->getVar('uid')));
-        $count = (int)($pm_handler->getCount($criteria));
-        $_SESSION['xoops_inbox_count'] = $count;
-        $_SESSION['xoops_inbox_count_expire'] = $time + 60;
+        $criteria = new CriteriaCompo(new Criteria('to_userid', $xoops->user->getVar('uid')));
+        $totals[$total] = $pmHandler->getCount($criteria);
+
+        $criteria->add(new Criteria('read_msg', 0));
+        $totals[$assign] = $pmHandler->getCount($criteria);
+
+        $session->set($inbox_count, $totals[$assign]);
+        $session->set($inbox_total, $totals[$total]);
+        $session->set($inbox_expire, $time + 60);
     }
-    if (!@empty($params['assign'])) {
-        $smarty->assign($params['assign'], $count);
-    } else {
-        echo $count;
+
+    $printCount = true;
+    foreach ($totals as $key => $count) {
+        if (!empty($params[$key])) {
+            $smarty->assign($params[$key], $count);
+            $printCount = false;
+        }
+    }
+    if ($printCount) {
+        echo $totals['assign'];
     }
 }
